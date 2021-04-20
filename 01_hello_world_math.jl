ff89510686f60c377119d76fea7c4bc02b4cc19e
mei <mei28aquarius@gmail.com>
2021-04-20 09:59:48 +0900

    ADD: hello world and basic math

---
+++ 01_hello_world_math.jl
@@ -0,0 +1,34 @@
+# hello world
+println("Hello World")
+
+hello_world = "hello world"
+println(hello_world)
+println(typeof(hello_world))
+
+# sum
+a = 10 + 7
+println(a)
+
+# diff
+a = 10 - 7
+println(a)
+
+# product
+a = 10 * 7
+println(a)
+
+# quotient 
+a = 10/7
+println(a)
+
+# quotient
+a = 7\10
+println(a)
+
+# power
+a = 10^7
+println(a)
+
+# modulus
+a = 10%7
+println(a)
